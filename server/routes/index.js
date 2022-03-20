85cdad5388a364a894fadf272fd21cde40ee8ecb
unknown <jjdate@live.com>
2022-03-20 16:19:31 -0400

    Created get route for business in toronto

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,27 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("Hello World!");
+router.get("/", async function (req, res, next) {
+  try {
+    axios
+      .get(`https://api.yelp.com/v3/businesses/search?location=Toronto`, {
+        headers: {
+          Authorization: `Bearer ${process.env.api_key}`,
+        },
+      })
+      .then((response) => {
+        res.json(response.data.businesses);
+      })
+      .catch((err) => {
+        console.log({ err: err });
+        res.status(400).json({ err });
+      });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 });
+
+router.get("/:id", async function (req, res, next) {});
 
 module.exports = router;
